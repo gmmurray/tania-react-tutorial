cc0576e0adbd2332f4650c590aedb1a60345a705
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2019-12-28 01:59:25 -0500

    Updates

---
+++ precache-manifest.36b2b35418a2a3e2206109b47bb15bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de3ef3008b2d379cf70065b3cacac5f",
+    "url": "/tania-react-tutorial/index.html"
+  },
+  {
+    "revision": "fc3d6b0f01f700531c08",
+    "url": "/tania-react-tutorial/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "3b6a5ee6c3b9e78b4150",
+    "url": "/tania-react-tutorial/static/js/2.8e167ac2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tania-react-tutorial/static/js/2.8e167ac2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc3d6b0f01f700531c08",
+    "url": "/tania-react-tutorial/static/js/main.8641e3dc.chunk.js"
+  },
+  {
+    "revision": "1b8068e7f73a4d424355",
+    "url": "/tania-react-tutorial/static/js/runtime-main.bcace2b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tania-react-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
